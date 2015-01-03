7ecb5fe00aa878577377a86f38cf46a8e9c0d532
MeLean <milen820430@gmail.com>
2015-01-03 12:50:34 +0200

    Routing works!!!

--- SPA_Project_adds-mainteiner/app/js/app.js
+++ SPA_Project_adds-mainteiner/app/js/app.js
@@ -1,2 +1,12 @@
 ﻿'use strict';
-var adsApp = angular.module('adsApp', []);
+var adsApp = angular.module('adsApp', ['ngRoute'])
+    .config(function ($routeProvider) {
+        $routeProvider 
+            .when('/', {
+                templateUrl: 'app/templates/home.html'
+            }).when('/login-form', {
+                templateUrl: 'app/templates/login-form.html'
+            }).when('/register-form', {
+                templateUrl: 'app/templates/register-form.html'
+            });
+});
