88d76b1ceb2acc923185a9c7266f454d03252dcd
MeLean <milen820430@gmail.com>
2015-01-03 11:29:21 +0200

    Prerouting

---
+++ SPA_Project_adds-mainteiner/app/js/controlers/adsControler.js
@@ -0,0 +1,7 @@
+﻿'use strict';
+adsApp.controller('AdsController', function($scope, adsData) {
+    var url = 'http://softuni-ads.azurewebsites.net/api/Ads'; // TODO edit it
+    adsData.getAds(url).then( function(data) {
+        $scope.Ads = data.ads;
+    });  
+});
