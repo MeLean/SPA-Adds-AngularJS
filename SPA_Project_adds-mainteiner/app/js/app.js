af8700665db22cb098694c3bcbaecb11b4c790c9
MeLean <milen820430@gmail.com>
2015-01-05 22:15:04 +0200

    working sorting and page filtering

--- SPA_Project_adds-mainteiner/app/js/app.js
+++ SPA_Project_adds-mainteiner/app/js/app.js
@@ -9,4 +9,5 @@ var adsApp = angular.module('adsApp', ['ngRoute'])
             }).when('/register-form', {
                 templateUrl: 'app/templates/register-form.html'
             }).otherwise({ redirectTo: 'app/templates/home.html'});
-});
+    })
+    .constant('baseUrl', 'http://softuni-ads.azurewebsites.net/api/');
--- SPA_Project_adds-mainteiner/app/js/controlers/mainController.js
+++ SPA_Project_adds-mainteiner/app/js/controlers/mainController.js
@@ -1,17 +1,8 @@
 ﻿'use strict';
-adsApp.controller('MainController', function ($scope, requestManager) {
-    var baseUrl = 'http://softuni-ads.azurewebsites.net/api/';
+adsApp.controller('MainController', function ($scope, requestManager, baseUrl) {
 
     requestManager.getDataFromUrl(baseUrl + 'ads').then(function (data) {
-        $scope.ads = data.ads;
-
-        var pages = data.numPages;
-        var pagesArr = [];
-        for (var i = 1; i <= pages; i++) {
-            pagesArr.push(i);
-        }
-
-        $scope.pagesArr = pagesArr;
+        displayAds(data);
     });
 
     requestManager.getDataFromUrl(baseUrl + 'towns').then(function (data) { 
@@ -21,4 +12,38 @@ adsApp.controller('MainController', function ($scope, requestManager) {
     requestManager.getDataFromUrl(baseUrl + 'categories').then(function (data) {
         $scope.categories = data;
     });
+
+    $scope.updateAds = function (categoryFilter, townFilter, startPage) {
+        var categoryUrlRequest = makeUrlForRequest(categoryFilter, 'CategoryId=');
+        var townRequest = makeUrlForRequest(townFilter, '&TownId=');
+        var pageRequest = startPage || 1;
+
+        console.log(baseUrl + 'Ads?' + categoryUrlRequest + townRequest + '&StartPage=' + startPage + '&PageSize=10'); //todo delete this
+        requestManager.getDataFromUrl(baseUrl + 'Ads?' + categoryUrlRequest + townRequest +
+            '&StartPage=' + pageRequest + '&PageSize=10')
+                   .then(function (data) {
+                       displayAds(data);
+                   });
+    }
+
+    function makeUrlForRequest(filter, str) {
+        if (filter) {
+            return str + filter;
+        } else {
+            return '';
+        }
+    }
+
+    function displayAds(data) {
+        var pages = data.numPages;
+        var pagesArr = [];
+
+        $scope.ads = data.ads;
+
+        for (var i = 1; i <= pages; i++) {
+            pagesArr.push(i);
+        }
+
+        $scope.pagesArr = pagesArr;
+    }
 });
