9a42305bf2973216d4ee53785761e2721d0a8858
MeLean <milen820430@gmail.com>
2015-01-06 23:33:36 +0200

    register is not working.

--- SPA_Project_adds-mainteiner/app/js/controlers/AdsController.js
+++
@@ -1,7 +0,0 @@
-﻿'use strict';
-adsApp.controller('AdsController', function($scope, adsData) {
-    var url = 'http://softuni-ads.azurewebsites.net/api/Ads'; // TODO edit it
-    adsData.getAds(url).then( function(data) {
-        $scope.Ads = data.ads;
-    });  
-});
--- SPA_Project_adds-mainteiner/app/js/services/requestManager.js
+++ SPA_Project_adds-mainteiner/app/js/services/requestManager.js
@@ -1,28 +1,37 @@
 ﻿'use strict';
 adsApp.factory('requestManager', function ($http, $q) {
-    var requester = function (url, method) {
+    var requester = function (obj, url, method) {
         var defer = $q.defer();
 
         $http({
             url: url,
             method: method,
+            data: obj
         }).success(function (data, status, headers, config) {
                 defer.resolve(data);
             })
             .error(function (data, status, headers, config) {
-                defer.reject(data);
-                console.log(data);
-                //TODO make notification
-            });
+                defer.reject(data);  
+        });
 
         return defer.promise;
     }
 
     var getDataFromUrl = function(databaseUrl) {
-        return requester(databaseUrl, 'GET');
-    };
+        return requester(null, databaseUrl, 'GET');
+    }
+
+    var register = function (obj, databaseUrl) {
+        return requester(obj, databaseUrl, 'POST');
+    }
+
+    var login = function (obj, databaseUrl) {
+        return requester(obj, databaseUrl, 'POST');
+    }
 
     return {
-        getDataFromUrl : getDataFromUrl
+        getDataFromUrl: getDataFromUrl,
+        register: register,
+        login:login
     }
 });
