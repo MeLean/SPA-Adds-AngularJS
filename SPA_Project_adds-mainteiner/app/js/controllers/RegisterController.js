b9c6da8a7515c1481ce99dbe99018c9460eb5d7f
MeLean <milen820430@gmail.com>
2015-01-07 23:56:59 +0200

    RegisterController.js almost ready!

--- SPA_Project_adds-mainteiner/app/js/controllers/RegisterController.js
+++ SPA_Project_adds-mainteiner/app/js/controllers/RegisterController.js
@@ -4,15 +4,21 @@ adsApp.controller('RegisterController', ['$scope', 'requestManager', 'baseUrl', 
     $scope.register = function() {
         var user = $scope.user;
         var url = baseUrl + 'user/register';
-        requestManager.register(user, url)
-            .then(function(data) {
-                messaging.successMsg('Registration successful!');
-                $scope.username = data.username;
-                $location.path('/');
-                console.log(data); // todo delete this
-            }, function (data) {
-                messaging.errorMsg('Registration failed! Details:' + JSON.stringify(data.modelState));
-                console.log(data); // todo delete this
-            });
+        if (user.password === user.confirmPassword) {
+            requestManager.register(user, url)
+                .then(function (data) {
+                    messaging.successMsg('Registration successful!');
+                    $scope.username = data.username;
+                    $location.path('/');
+                    console.log(data); // todo delete this
+                }, function (data) {
+                    var details = JSON.stringify(data.modelState);
+                    var msgDetails = details.slice(5, (details.length - 2));
+                    messaging.errorMsg('Registration failed! Details: ' + msgDetails); 
+                });
+        } else {
+            messaging.errorMsg('Passwords do not match!');
+        }
+
     }
 }]);
