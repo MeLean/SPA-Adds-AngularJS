33aeaa742afa92c66bed20692301fb97c71729ab
MeLean <milen820430@gmail.com>
2015-01-11 14:40:17 +0200

    I have to stop looking for a spaces. Saw more and they are removed!

--- SPA_Project_adds-mainteiner/app/js/controllers/mainController.js
+++ SPA_Project_adds-mainteiner/app/js/controllers/mainController.js
@@ -7,29 +7,28 @@ adsApp.controller('MainController', ['$scope', 'requestManager', 'baseUrl', 'mes
         });
 
         requestManager.getDataFromUrl(baseUrl + 'towns').then(function (data) {
-        $scope.towns = data;
-    });
+            $scope.towns = data;
+        });
 
         requestManager.getDataFromUrl(baseUrl + 'categories').then(function (data) {
-        $scope.categories = data;
-    });
+            $scope.categories = data;
+        });
 
         $scope.updateAds = function (categoryFilter, townFilter, startPage, adsPerPage) {
-        var categoryUrlRequest = makeUrlForRequest(categoryFilter, 'CategoryId=');
-        var townRequest = makeUrlForRequest(townFilter, '&TownId=');
-        var pageRequest = startPage || 1;
-        var pageSizeRequest = adsPerPage || 10;
+            var categoryUrlRequest = makeUrlForRequest(categoryFilter, 'CategoryId=');
+            var townRequest = makeUrlForRequest(townFilter, '&TownId=');
+            var pageRequest = startPage || 1;
+            var pageSizeRequest = adsPerPage || 10;
 
         requestManager.getDataFromUrl(baseUrl + 'Ads?' + categoryUrlRequest + townRequest +
-            '&StartPage=' + pageRequest + '&PageSize=' + pageSizeRequest)
-                   .then(
-                       function (data) {
-                           displayAds(data);
-                       },
-                       function (data) {
-                           messaging.errorMsg('request failed');
-                           console.log(data);
-                       });
+            '&StartPage=' + pageRequest + '&PageSize=' + pageSizeRequest).then(
+                function (data) {
+                    displayAds(data);
+                },
+                function (data) {
+                    messaging.errorMsg('request failed');
+                    console.log(data);
+                });
         }  
 
         $scope.checkForLogin = function () {
@@ -45,29 +44,29 @@ adsApp.controller('MainController', ['$scope', 'requestManager', 'baseUrl', 'mes
         }
 
         function makeUrlForRequest(filter, str) {
-        if (filter) {
-            return str + filter;
-        } else {
-            return '';
-        }
+            if (filter) {
+                return str + filter;
+            } else {
+                return '';
+            }
     }
 
         function displayAds(data)  {
-        var ads = data.ads;
-        if (ads.length > 0) {
-            var pages = data.numPages;
-            var pagesArr = [];
+            var ads = data.ads;
+            if (ads.length > 0) {
+                var pages = data.numPages;
+                var pagesArr = [];
 
-            $scope.ads = ads;
+                $scope.ads = ads;
 
-            for (var i = 1; i <= pages; i++) {
-                pagesArr.push(i);
-            }
+                for (var i = 1; i <= pages; i++) {
+                    pagesArr.push(i);
+                }
 
-            $scope.pagesArr = pagesArr;
-        } else {
-            $scope.ads = ads; 
-            messaging.errorMsg('There are no such ads!');
+                $scope.pagesArr = pagesArr;
+            } else {
+                $scope.ads = ads; 
+                messaging.errorMsg('There are no such ads!');
         }
     }
 }]);
